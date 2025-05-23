24510ede6cc9b204ca8d3cdd49f5f58714a9f4db
sydulamin10 <sydul.cit.bd@gmail.com>
2025-05-23 20:48:35 +0600

    newly added

--- home/views.py
+++ home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from Product.models import Product
 # Create your views here.
 
 # @login_required(login_url='reg')
 def home(request):
-    return render(request, 'home/home.html')
+
+    new_arivel = Product.objects.filter(new_arrival=True)
+    top_rated = Product.objects.filter(top_rated=True)
+
+    return render(request, 'home/home.html', {'new_arivel': new_arivel, 'top_rated': top_rated})
